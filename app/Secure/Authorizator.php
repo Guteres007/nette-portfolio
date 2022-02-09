6d4dd24eef0c2fff6fc03041312a9362ba19ca6d
Andrasi <andrasi.m007@gmail.com>
2022-02-09 13:54:52 +0100

    oprávnění

---
+++ app/Secure/Authorizator.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Secure;
+
+
+class Authorizator implements \Nette\Security\Authorizator
+{
+
+    function isAllowed($role, $resource, $operation): bool
+    {
+
+        if ($role === 'admin') {
+            return true;
+        }
+        if ($role === 'user' && $operation === 'create') {
+            return true;
+        }
+        return false;
+    }
+}
